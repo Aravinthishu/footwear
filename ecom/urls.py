2ed450036e541cdb64e2cbee8a703f89b9520eac
Aravinthishu <aravintharavinth7746@gmail.com>
2024-10-04 11:44:08 +0530

    update settings.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,5 @@ urlpatterns = [
     path('product/', include('product.urls')),
     path('category/', include('category.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
